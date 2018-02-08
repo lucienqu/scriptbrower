48c371047d179775f1b17112870ce0e1e9849e9c
lucienqu <35301885+lucienqu@users.noreply.github.com>
2018-02-08 17:47:45 +0800

    Initial commit

---
+++ 无标题1.ps1
@@ -0,0 +1,18 @@
+﻿find-module ScriptBrowser | fl
+
+help save-module -Online
+
+ScriptBrowser
+
+Find-Module -Name "ScriptBrowser" -Repository "PSGallery" | Save-Module -Path D:\NF0281\Documents\
+
+Install-Module -Name ScriptBrowser
+
+get-command *script*  | Sort-Object source
+
+
+Enable-ScriptBrowser                               
+Enable-ScriptAnalyzer                              
+Disable-ScriptBrowser                              
+Start-ScriptBrowserDesktop                         
+Disable-ScriptAnalyzer                             
